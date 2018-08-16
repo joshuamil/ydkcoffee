682f3818eab614276fdb38ad3cacaed096958eaf
Joshua Lawson <ordinarycryptid@gmail.com>
2018-08-16 08:05:43 -0400

    reviewed and added helpful commentary (for myself)

--- assets/scripts/recipe.js
+++ assets/scripts/recipe.js
@@ -21,6 +21,11 @@ export default class Recipe {
     // Passed-in name from navigation text
     // should match the name="" property from the JSON file
     console.log(this.data.name + '==' + method);
+
+    /* this.data.name is nothing because we first have to drill
+    down into the array of recipes. Then we can compare 
+    this.data[i].name to method. First step is to figure out
+    the best way to do that.*/
     
     // Retrieve values from the recipe and write the values
     // into the main article tag inside of /assets/templates/main.html
